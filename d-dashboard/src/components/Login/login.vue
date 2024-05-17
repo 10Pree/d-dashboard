d238c666075a7d2ee298cf6bd3210bae895c423f
Nonthanan <nonthanansimpree@gmail.com>
2024-05-17 15:58:42 +0700

    add img Logo
    add img Logo

--- d-dashboard/src/components/Login/login.vue
+++ d-dashboard/src/components/Login/login.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Login</h1>
     <div>
+      <img src="../src/assets/images/logo_1.png" alt="Logo">
         <input type="text" name="" id="">
         <button class="btn btn-primary">Primary Button</button>
         <i class="fas fa-home"></i>
