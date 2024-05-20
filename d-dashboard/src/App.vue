bbb1cb69092095cd9895a1b037f2a7835f2c7c12
Nonthanan <nonthanansimpree@gmail.com>
2024-05-20 15:40:41 +0700

    install vue-router
    install vue-router

--- d-dashboard/src/App.vue
+++ d-dashboard/src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import Dashboard from './components/Dashboard/dashboard.vue';
+import dashboard from './components/Dashboard/dashboard.vue'
 import Login from './components/Login/login.vue'
-import Register from './components/Register/register.vue';
 </script>
 
 <template>
-    <Dashboard/>
+<login/>
 </template>
 
 <style>
