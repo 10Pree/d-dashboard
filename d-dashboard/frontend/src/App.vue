bdada0f78f9e74a86a6eacc418bc9422b493309a
Nonthanan <nonthanansimpree@gmail.com>
2024-08-05 18:20:34 +0700

    Update App.vue

--- d-dashboard/frontend/src/App.vue
+++ d-dashboard/frontend/src/App.vue
@@ -3,6 +3,7 @@ import dashboard from "./components/Dashboard/dashboard.vue";
 import Login from "./components/Login/login.vue";
 </script>
 <template>
+      <h1>wdwdwd</h1>
       <router-view></router-view>
 </template>
 <style></style>
